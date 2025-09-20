833165e502179d7b8500ef581384eb43e5b9ddb4
amrattategar30 <amrattategar30@gmail.com>
2025-09-20 21:25:51 +0000

    add: category (finished)

--- src/components/view/LandingPage/Category.tsx
+++ src/components/view/LandingPage/Category.tsx
@@ -5,7 +5,8 @@ import { LuArrowUpRight } from "react-icons/lu";
 
 function Category() {
   return (
-    <div className="md:px-10 py:20 ">
+    <div className="md:px-10 my-24 ">
+      {/* row 1 */}
       <div className="flex justify-between items-center mb-20">
         <h3 className="w-1/5 text-3xl font-medium">02</h3>
         <h2 className="w-3/5 font-didot text-8xl uppercase text-center">
@@ -15,30 +16,48 @@ function Category() {
           All grilled to perfection over charcoal our dips and sauces
         </p>
       </div>
-      <div className="flex justify-between my-10">
+      {/* row 2 */}
+      <div className="flex justify-between mb-20">
         <Image
           src={imageAsset.food4}
           isBlurred
           className="w-[200px] md:w-[300px] object-cover"
         />
-        <div>
-          <div className="flex flex-col">
-            {MENU_ITEMS.map((item) => (
-              <div className="w-[500px] ">
-                <div className="my-3 relative">
-                  <p className="text-xl ">{item.label}</p>
-                  <LuArrowUpRight className="absolute size-9 -top-3 right-0" />
-                </div>
-                <hr className="border-t-2 border-black" />
+        <div className="flex flex-col">
+          {MENU_ITEMS.map((item) => (
+            <div className="w-[500px] ">
+              <div className="my-3 relative">
+                <p className="text-xl ">{item.label}</p>
+                <LuArrowUpRight className="absolute size-9 -top-3 right-0" />
               </div>
-            ))}
-          </div>
+              <hr className="border-t-2 border-black" />
+            </div>
+          ))}
         </div>
         <Image
           src={imageAsset.food5}
           isBlurred
           className="w-[200px] md:w-[300px] object-cover"
         />
+      </div>
+      {/* row 3 */}
+      <div className="flex justify-between items-center gap-20  ">
+        <p className="w-3/6 text-xl">
+          We understand that every event is unique, and we work closely with you
+          to customize our catering menu to suit ypur specific needs.
+        </p>
+        <div className="w-3/6 flex justify-center">
+          <Image
+            src={imageAsset.food6}
+            isBlurred
+            className="w-[200px] md:w-[300px] object-cover"
+          />
+        </div>
+        <div className="w-3/6 flex justify-center">
+          <button className="size-28 md:size-40 rounded-full bg-slate-950 hover:bg-slate-800 flex justify-center items-center relative p-5 text-sm md:text-base text-white text-center font-light ">
+            Explore More
+          </button>
+        </div>
       </div>
     </div>
   );
